27b0dcd2cfcab5a4c1fcaa6efdea2d2ece4c68b7
danghoangthien <danghoangthien@gmail.com>
2022-09-27 22:48:23 +0700

    Updates

--- client/src/app/pages/HomePage/HomePage.style.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/macro';
-import { StyleConstants, ScreenSizes } from 'styles/StyleConstants';
-
-export const HomePageLayout = styled.div`
-    width: 100%;
-`;
--- client/src/hook/useMountEffect.tsx
+++
@@ -1,2 +0,0 @@
-import { useEffect } from 'react';
-export const useMountEffect = fun => useEffect(fun, []);
