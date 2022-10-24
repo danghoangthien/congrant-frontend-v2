128b6d26c2e0a851f97157702d47fecbb23469e7
danghoangthien <danghoangthien@gmail.com>
2022-10-24 11:07:30 +0700

    fix: update DrawerHandle behave (cont)

--- client/src/app/pages/SupporterPage/components/SupporterInfo.jsx
+++ client/src/app/pages/SupporterPage/components/SupporterInfo.jsx
@@ -23,10 +23,12 @@ const SupporterInfo = ({ closeDrawer }) => {
             </Col>
             <Col>
               <Row align="middle">
-                <Button className="mr-5 icon-btn" icon={<EmailIcon className="mr-2" />}>
+                <Button className="icon-btn" icon={<EmailIcon className="mr-2 pointer" />}>
                   {'メールを送る'}
                 </Button>
-                <CloseIcon onClick={() => closeDrawer()} />
+                <Button type="text">
+                  <CloseIcon className="pointer" onClick={() => closeDrawer()} />
+                </Button>
               </Row>
             </Col>
           </Row>
